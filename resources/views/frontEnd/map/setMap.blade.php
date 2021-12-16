27bd030055f03616e9c94d96ab2811d1db83f86e
Rajwan ullah <rajwan.diu@gmail.com>
2021-12-16 21:04:43 +0600

    first commit

--- resources/views/frontEnd/map/setMap.blade.php
+++ resources/views/frontEnd/map/setMap.blade.php
@@ -18,7 +18,7 @@
 
 <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCFuy2ZyCtgb54dHAro5CZ7h1HhEmqddpM&libraries=places"
+<script src="https://maps.googleapis.com/maps/api/js?key=#&libraries=places"
   type="text/javascript"></script>
 
 <div class="container">
